53e5b716cb4333b755d115cd8949b4fbb88d24bd
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2024-01-28 08:44:54 +0800

    feat: Add initial implementation of a method that places ship on the grid

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -4,12 +4,27 @@ export default class Gameboard {
         for (let i = 0; i < 10; i++) {
             let row = [];
             for (let j = 0; j < 10; j++) {
-                row.push({ isHit: false });
+                row.push({ isHit: false, ship: null });
             }
             this.grid.push(row);
         }
     }
+
     receiveAttack(x, y) {
         this.grid[x][y].isHit = true;
+    }
+
+    placeShip(Ship, [initialRow, initialColumn]) {
+        try {
+            for (let i = 0; i < Ship.length; i++) {
+                if (Ship.isVertical) {
+                    this.grid[initialRow + i][initialColumn].ship = Ship;
+                } else {
+                    this.grid[initialRow][initialColumn + i].ship = Ship;
+                }
+            }
+        } catch (error) {
+            throw new Error('Invalid coordinates');
+        }
     }
 }
--- src/Gameboard.test.js
+++ src/Gameboard.test.js
@@ -1,4 +1,5 @@
 import Gameboard from './Gameboard';
+import Ship from './Ship';
 
 test('Unhit grids will have a false "isHit" value', () => {
     let gameboard = new Gameboard();
@@ -22,4 +23,75 @@ test('Hit grids will have a true "isHit" value', () => {
     gameboard.receiveAttack(0, 1);
     gameboard.receiveAttack(0, 2);
     expect(gameboard.grid[0][0].isHit && gameboard.grid[0][1].isHit && gameboard.grid[0][2].isHit).toBe(true);
+});
+
+test('Ships are placed at the right coordinates', () => {
+    // Create gameboard.
+    const gameboard = new Gameboard();
+
+    // Create a size 4 vertical ship.
+    const shipLength = 4;
+    const isVertical = true;
+    const ship = new Ship(shipLength, isVertical);
+
+    // Place the ship.
+    const initialX = 0;
+    const initialY = 1;
+    gameboard.placeShip(ship, [initialX, initialY]);
+
+    // Get all the grids occupied by the ship
+    const occupiedGrids = [];
+    for (let i = 0; i < shipLength; i++) {
+        if (ship.isVertical) {
+            occupiedGrids.push(gameboard.grid[initialX + i][initialY]);
+        } else {
+            occupiedGrids.push(gameboard.grid[initialX][initialY + 1]);
+        }
+    }
+
+    expect(occupiedGrids.every((grid) => grid.ship === ship)).toBe(true);
+});
+
+test('Place ship method throws error if a vertical ship is placed in an invalid coordinates ', () => {
+    // Create gameboard.
+    const gameboard = new Gameboard();
+
+    // Create a size 4 vertical ship.
+    const shipLength = 4;
+    const isVertical = true;
+    const ship = new Ship(shipLength, isVertical);
+
+    // Place the ship.
+    const initialX = 9;
+    const initialY = 9;
+
+    // Wrap the placeShip operation in a function
+    const placeShipOperation = () => {
+        gameboard.placeShip(ship, [initialX, initialY]);
+    };
+
+    // Use the expect statement with the wrapped function
+    expect(placeShipOperation).toThrow(/Invalid/);
+});
+
+test('Place ship method throws an error if a horizontal ship is placed in an invalid coordinates ', () => {
+    // Create gameboard.
+    const gameboard = new Gameboard();
+
+    // Create a size 2 horizontal ship.
+    const shipLength = 2;
+    const isVertical = false;
+    const ship = new Ship(shipLength, isVertical);
+
+    // Place the ship.
+    const initialX = 0;
+    const initialY = 9;
+
+    // Wrap the placeShip operation in a function
+    const placeShipOperation = () => {
+        gameboard.placeShip(ship, [initialX, initialY]);
+    };
+
+    // Use the expect statement with the wrapped function
+    expect(placeShipOperation).toThrow(/Invalid/);
 });
