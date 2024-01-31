004841c50d6692189d95f96c0114795c375b05c3
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2024-02-01 06:50:11 +0800

    feat: Create list of possible game modes with current game mode currently set to single player for now

---
+++ src/runGame.js
@@ -0,0 +1,6 @@
+export default function runGame() {
+    const gameModes = ['single-player-1-device', 'multiplayer-1-device', 'multiplayer-2-device'];
+
+    // Pick a game mode (set to single player for now).
+    const currentGameMode = gameModes[0];
+}
