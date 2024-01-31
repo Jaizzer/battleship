f8f0aadd0e2acc3d8f9c946c5d530acd3fcf827b
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2024-02-01 06:13:17 +0800

    feat: Create Player object

---
+++ src/Player.js
@@ -0,0 +1,7 @@
+export default class Player {
+    constructor(name, gameboard, isComputer) {
+        this.name = name;
+        this.gameboard = gameboard;
+        this.isComputer = isComputer;
+    }
+}
