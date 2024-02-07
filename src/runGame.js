0d7e557a03a866a4478880fa6747f982f3861e99
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2024-02-08 06:41:48 +0800

    feat: Initialize test values for player B

--- src/runGame.js
+++ src/runGame.js
@@ -25,4 +25,21 @@ export default function runGame() {
     ];
     let playerAGameboard = randomlyPlaceFleet(playerAFleet, new Gameboard(10));
     const playerA = new Player(playerAName, playerAGameboard, false);
+
+    // Create Player B.
+    const playerBName = 'Computer';
+    const playerBFleet = [
+        new Ship(1, 'vertical'),
+        new Ship(1, 'vertical'),
+        new Ship(1, 'vertical'),
+        new Ship(1, 'vertical'),
+        new Ship(2, 'horizontal'),
+        new Ship(2, 'horizontal'),
+        new Ship(2, 'horizontal'),
+        new Ship(3, 'vertical'),
+        new Ship(3, 'horizontal'),
+        new Ship(4, 'horizontal'),
+    ];
+    let playerBGameboard = randomlyPlaceFleet(playerBFleet, new Gameboard(10));
+    const playerB = new Player(playerBName, playerBGameboard, false);
 }
