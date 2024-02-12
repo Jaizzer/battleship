7741a862e8c48520f1a197cb8bc373bae95dc941
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2024-02-13 07:18:23 +0800

    feat: Implement a function that waits for the player to select a grid and return the corresponding coordinates of that grid

--- src/startGame.js
+++ src/startGame.js
@@ -49,3 +49,15 @@ export default function startGame(playerA, playerB) {
 
     return winner;
 }
+
+// Function that waits for the player to select a grid and return the corresponding coordinates of that grid.
+async function getGridCoordinatesToAttack(gameboard) {
+    return new Promise((resolve) => {
+        const grids = Array.from(gameboard.children);
+        grids.forEach((grid) => {
+            grid.addEventListener('click', () => {
+                resolve([parseInt(grid.id[0]), parseInt(grid.id[2])]);
+            });
+        });
+    });
+}
