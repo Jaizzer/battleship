46b52fa5095cbdc687e30629705c21887bcf29b5
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2024-02-19 06:54:35 +0800

    feat: Add classes to some elemnets

--- src/getGameMode.js
+++ src/getGameMode.js
@@ -1,10 +1,12 @@
 export default async function getGameMode(gameModes) {
     // Create the main container of the prompt that will ask user the game mode.
     const gameModePromptContainer = document.createElement('div');
+    gameModePromptContainer.classList.add('game-mode-prompt');
     document.body.appendChild(gameModePromptContainer);
 
     // Create the title of the prompt.
     const promptTitle = document.createElement('div');
+    promptTitle.classList.add('title');
     promptTitle.textContent = 'Choose Game Mode';
     gameModePromptContainer.appendChild(promptTitle);
 
@@ -27,6 +29,7 @@ export default async function getGameMode(gameModes) {
 
     // Add the submit button to the form.
     const submitButton = document.createElement('button');
+    submitButton.classList.add('submit-button');
     submitButton.textContent = 'Start Game';
     submitButton.type = 'submit';
     form.appendChild(submitButton);
