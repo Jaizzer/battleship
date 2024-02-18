668fc7ec209df2bea02d532dc3c7829e8207e86d
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2024-02-19 07:53:26 +0800

    feat: Implement a function that creates players through DOM inputs

---
+++ src/createPlayer.js
@@ -0,0 +1,81 @@
+import Gameboard from './Gameboard';
+import Player from './Player';
+import randomlyPlaceFleet from './randomlyPlaceFleet';
+import Ship from './Ship';
+
+export default function createPlayer(playerIsComputer) {
+    // Create the fleet of the player.
+    const playerFleet = [
+        new Ship(1, 'vertical'),
+        new Ship(1, 'vertical'),
+        new Ship(1, 'vertical'),
+        new Ship(1, 'vertical'),
+        new Ship(2, 'horizontal'),
+        new Ship(2, 'horizontal'),
+        new Ship(2, 'horizontal'),
+        new Ship(3, 'vertical'),
+        new Ship(3, 'horizontal'),
+        new Ship(4, 'horizontal'),
+    ];
+
+    // Create the gameboard.
+    const playerGameboard = randomlyPlaceFleet(playerFleet, new Gameboard(10));
+
+    if (!playerIsComputer) {
+        // If player is not computer, render the form.
+
+        // Create main container for creating player.
+        const playerCreationPrompt = document.createElement('div');
+        playerCreationPrompt.classList.add('player-creation-prompt');
+        document.body.appendChild(playerCreationPrompt);
+
+        // Create prompt title.
+        const title = document.createElement('div');
+        title.classList.add('title');
+        title.innerHTML = `Choose Username`;
+        playerCreationPrompt.appendChild(title);
+
+        // Create the form.
+        const form = document.createElement('form');
+        playerCreationPrompt.appendChild(form);
+
+        // Create the input for name.
+        const nameInput = document.createElement('input');
+        nameInput.id = 'name';
+        nameInput.type = 'text';
+        nameInput.maxLength = 10;
+        nameInput.placeholde = 'Player Name';
+        form.appendChild(nameInput);
+
+        // Add label
+        const nameInputLabel = document.createElement('label');
+        nameInputLabel.setAttribute('for', 'name');
+        nameInputLabel.textContent = 'Name';
+        form.appendChild(nameInputLabel);
+
+        // Add the submit button to the form.
+        const submitButton = document.createElement('button');
+        submitButton.classList.add('submit-button');
+        submitButton.textContent = 'Create';
+        submitButton.type = 'submit';
+        form.appendChild(submitButton);
+
+        return new Promise((resolve) => {
+            form.addEventListener('submit', (event) => {
+                // Prevent form submission from reloading the page.
+                event.preventDefault();
+
+                // Remove the form in the DOM.
+                document.body.innerHTML = '';
+
+                // Get player name from the form.
+                const playerName = nameInput.value;
+
+                resolve(new Player(playerName, playerGameboard, playerIsComputer));
+            });
+        });
+    } else {
+        // If the player is a computer, no need to render the DOM.
+        return new Player('Computer', playerGameboard, playerIsComputer);
+    }
+}
