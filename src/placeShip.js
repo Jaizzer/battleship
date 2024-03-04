cc921f0922eb5078ff30c8dedde8545b2cf85b9f
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2024-03-04 23:38:44 +0800

    docs: Remove periods in comments

--- src/placeShip.js
+++ src/placeShip.js
@@ -33,17 +33,17 @@ export default async function placeShip() {
         shipDiv.draggable = true;
 
         shipDiv.addEventListener('dragstart', (event) => {
-            // Set the cursor position to the bottom-left corner of the ship div.
+            // Set the cursor position to the bottom-left corner of the ship div
             const offsetX = 15;
             const offsetY = shipDiv.clientHeight - 20;
 
-            // Set the drag image offset to the cursor position.
+            // Set the drag image offset to the cursor position
             event.dataTransfer.setDragImage(shipDiv, offsetX, offsetY);
 
             // Get the current ship being dragged.
             let selected = shipDiv;
 
-            // Store reference to the ships current grid container.
+            // Store reference to the ships current grid container
             const currentGridContainer = shipDiv.parentElement;
 
             // Access all the grids in the gameboard.
@@ -53,7 +53,7 @@ export default async function placeShip() {
                     event.preventDefault();
                 });
                 grid.addEventListener('drop', () => {
-                    // Don't drop ship is location is invalid.
+                    // Don't drop ship if location is invalid
                     try {
                         // If the ship that was dropped came from a previous grid, remove the ship from that previous grid
                         if (currentGridContainer !== null && currentGridContainer.classList.contains('gameboard-grid')) {
@@ -62,11 +62,11 @@ export default async function placeShip() {
                             playerGameboard.removeShipAt([parseInt(previousX), parseInt(previousY)]);
                         }
 
-                        // Place the ship to the new grid container.
+                        // Place the ship to the new grid container
                         let [newX, newY] = grid.id.split('-');
                         playerGameboard.placeShip(ship, [parseInt(newX), parseInt(newY)]);
 
-                        // Put the ship starting from currently selected grid.
+                        // Put the ship starting from currently selected grid
                         grid.appendChild(selected);
                     } catch (error) {
                         // Place the ship back to its previous grid if the ship dropping attempt failed on the new grid
@@ -85,16 +85,16 @@ export default async function placeShip() {
             // Access all the grids in the gameboard.
             let grids = [...gameboardForDOM.childNodes];
 
-            // Clear event listeners to all grid after a ship is succesfully dropped to a particular grid to prevent muliple attachment of event grids.
+            // Clear event listeners to all grid after a ship is succesfully dropped to a particular grid to prevent muliple attachment of event grids
             grids.forEach((currentGrid) => {
-                // Create a copy of a grid with no event listener.
+                // Create a copy of a grid with no event listener
                 const noEventListenerGrid = currentGrid.cloneNode(true);
-                // Don't replace the ship inside the grid to preserve the ship's dragstart event.
+                // Don't replace the ship inside the grid to preserve the ship's dragstart event
                 if (currentGrid.firstChild) {
                     const originalShip = currentGrid.firstChild;
                     noEventListenerGrid.replaceChild(originalShip, noEventListenerGrid.firstChild);
                 }
-                // Put the grid with no event listener to the gameboard.
+                // Put the grid with no event listener to the gameboard
                 currentGrid.parentNode.replaceChild(noEventListenerGrid, currentGrid);
             });
         });
