8a20fcf425108abc894c72b5d868fdff092f4a95
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2024-03-13 13:22:19 +0800

    refac: Use the createCopyWithNoEventListener function to remoe the events of the nodes

--- src/placeShip.js
+++ src/placeShip.js
@@ -107,33 +107,14 @@ export default async function placeShip() {
             // Clear event listeners to all grid after a ship is succesfully dropped to a particular grid to prevent muliple attachment of event grids
             grids.forEach((currentGrid) => {
                 // Create a copy of a grid with no event listener
-                const noEventListenerGrid = currentGrid.cloneNode(true);
-                // Don't replace the ship inside the grid to preserve the ship's dragstart event
-                if (currentGrid.firstChild) {
-                    const originalShip = currentGrid.firstChild;
-                    noEventListenerGrid.replaceChild(originalShip, noEventListenerGrid.firstChild);
-                }
-                // Put the grid with no event listener to the gameboard
-                currentGrid.parentNode.replaceChild(noEventListenerGrid, currentGrid);
+                const eventlessGrid = createCopyWithNoEventListener(currentGrid, true);
+                currentGrid.parentNode.replaceChild(eventlessGrid, currentGrid);
             });
 
-            // Create function that removes fleet container's event listeners
-            const removeFleetContainerEventListener = function () {
-                // Replace the original fleet container with a copy that has no event listeners.
-                const noEventListenerFleetContainer = fleetContainer.cloneNode(false);
-                fleetContainer.parentNode.replaceChild(noEventListenerFleetContainer, fleetContainer);
-
-                // Retain the original ships inside the original fleet container.
-                [...fleetContainer.childNodes].forEach((ship) => {
-                    noEventListenerFleetContainer.appendChild(ship);
-                });
-
-                // Set the cloned fleet container as now the original fleet container
-                fleetContainer = noEventListenerFleetContainer;
-            };
-
-            // Remove fleet container event listener everytime drag ends to prevent stacking multiple event listener
-            removeFleetContainerEventListener();
+            // Clear all event listeners in the fleet container to prevent events from stacking together.
+            const eventlessFleetContainer = createCopyWithNoEventListener(fleetContainer, true);
+            fleetContainer.parentNode.replaceChild(eventlessFleetContainer, fleetContainer);
+            fleetContainer = eventlessFleetContainer;
         });
 
         fleetContainer.appendChild(shipDiv);
