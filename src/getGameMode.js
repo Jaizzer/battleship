b3562a7982ced22de42fd6bf1cbf3f091094dda0
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2024-03-18 06:31:46 +0800

    feat: Add text and icon to gamemode selections

--- src/getGameMode.js
+++ src/getGameMode.js
@@ -1,3 +1,7 @@
+import onePlayerIcon from './img/1-player.png';
+import twoPlayerIcon from './img/2-player.png';
+import globalIcon from './img/global.png';
+
 export default async function getGameMode(gameModes) {
     // Create the main container of the prompt that will ask user the game mode.
     const gameModePromptContainer = document.createElement('div');
@@ -10,17 +14,31 @@ export default async function getGameMode(gameModes) {
     promptTitle.textContent = 'Choose Game Mode';
     gameModePromptContainer.appendChild(promptTitle);
 
+    // Crate array of icons
+    const icons = [onePlayerIcon, twoPlayerIcon, globalIcon];
+
     // Create divs corresponding to different game modes
     const gameModeDivs = [];
-    gameModes.forEach((gameMode) => {
+    for (let i = 0; i < gameModes.length; i++) {
         const gameModeDiv = document.createElement('div');
-        gameModeDiv.classList.add(gameMode);
+        gameModeDiv.classList.add(gameModes[i]);
 
+        // Add the icon
+        const icon = document.createElement('img');
+        icon.classList.add('icon');
+        icon.src = icons[i];
+        gameModeDiv.appendChild(icon);
+
+        // Add game mode text.
+        const text = document.createElement('div');
+        text.classList.add('text');
         // Replace hyphens with spaces and capitalize each first word since 'gameModes' contain string that are hyphenated.
-        gameModeDiv.textContent = gameMode.replace(/-/g, ' ').replace(/\b\w/g, (match) => match.toUpperCase());
+        text.textContent = gameModes[i].replace(/-/g, ' ').replace(/\b\w/g, (match) => match.toUpperCase());
+        gameModeDiv.appendChild(text);
+
         gameModePromptContainer.appendChild(gameModeDiv);
         gameModeDivs.push(gameModeDiv);
-    });
+    }
 
     return new Promise((resolve) => {
         gameModeDivs.forEach((gameModeDiv) => {
