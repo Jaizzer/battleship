d462eca8f0b764569aee8de2512e1b95f89226a8
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2024-04-08 16:15:57 +0800

    feat: Create module for popping up message

---
+++ src/popUpMessage.js
@@ -0,0 +1,34 @@
+export default function popUpMessage(messageHeading, message) {
+    // Create background container
+    const backgroundContainer = document.createElement('div');
+    backgroundContainer.className = 'blurry-background';
+    document.body.appendChild(backgroundContainer);
+
+    // Create message container
+    const messageContainer = document.createElement('div');
+    messageContainer.className = 'message-container';
+    backgroundContainer.appendChild(messageContainer);
+
+    // Create heading
+    const heading = document.createElement('div');
+    heading.className = 'message-heading';
+    heading.textContent = messageHeading;
+    messageContainer.appendChild(heading);
+
+    // Create message
+    const text = document.createElement('text');
+    text.className = 'popup-text';
+    text.textContent = message;
+    messageContainer.appendChild(text);
+
+    // Create close button.
+    const closeButton = document.createElement('button');
+    closeButton.classList.add('close');
+    closeButton.textContent = 'Back to Game';
+    messageContainer.appendChild(closeButton);
+
+    // Add event listener to the close div
+    closeButton.addEventListener('click', () => {
+        document.body.removeChild(backgroundContainer);
+    });
+}
